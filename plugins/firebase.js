5042cf6b2bdbf0e459c2999282e31edd6459c90d
hattorishota <hattorishota.eng@gmail.com>
2022-05-06 09:47:00 +0900

    first commit

---
+++ plugins/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBmRAL1SbKNnYzAuFSN8UgamTjngyB7CDQ",
+  authDomain: "jimoguru-db.firebaseapp.com",
+  databaseURL: "https://jimoguru-db-default-rtdb.firebaseio.com",
+  projectId: "jimoguru-db",
+  storageBucket: "jimoguru-db.appspot.com",
+  messagingSenderId: "1061260449171",
+  appId: "1:1061260449171:web:fd7732ce563e0e17b24466",
+  measurementId: "G-NGSVVVVMK2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
